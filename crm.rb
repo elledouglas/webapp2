2382f38012ad8aa331b576ee25679450b67c1d86
Kathleen <kathleenmcclay@gmail.com>
2017-06-05 12:57:01 -0400

    3rd commit

--- crm.rb
+++ crm.rb
@@ -3,8 +3,9 @@ require_relative './contact'
 
 
 
-get '/home' do
+get '/' do
 erb :index
+redirect '/contacts'
 end
 
 # get '/contacts' do
@@ -25,7 +26,7 @@ end
  end
 
 get '/contacts/:id' do
-  @contact = Contact.find_by(id: params[:id].to_i)
+  @contact = Contact.find_by(id: params[:id])
   if @contact
     erb :show_contact
   else
@@ -42,6 +43,23 @@ post '/contacts' do
     note:       params[:note]
   )
   redirect to ('/contacts')
+end
+
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id])
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 after do
