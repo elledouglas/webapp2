a362d1a10004cc3ea7307d122d6d79ad06667279
Kathleen <kathleenmcclay@gmail.com>
2017-06-05 12:18:41 -0400

    first commit

--- crm.rb
+++ crm.rb
@@ -9,25 +9,31 @@ end
 
 # get '/contacts' do
 #   @contact = ['first_name', 'last_name', 'email', 'note']
-# erb:contacts
-# end
+ #erb:contacts
+ #end
 
 # get 'contacts/:id' do
 # 'params[:id]'
 # end
+get '/contacts/new' do
+erb :new
+end
 
  get '/contacts' do
  @contacts = Contact.all
  erb :contacts
  end
+
 get '/contacts/:id' do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
     erb :show_contact
   else
     raise Sinatra::NotFound
   end
 end
+
+
 
 after do
   ActiveRecord::Base.connection.close
